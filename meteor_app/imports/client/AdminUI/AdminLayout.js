84032a4d69e290bbf0af1f74a87470513a98b25c
Cláudio Sá de Castro <claudio@hiades.com.br>
2018-02-13 10:28:41 -0200

    Reformat code according to recommended code style, and get rid of superfluous code.

--- meteor_app/imports/client/AdminUI/AdminLayout.js
+++ meteor_app/imports/client/AdminUI/AdminLayout.js
@@ -41,22 +41,17 @@ import './LoginBtn.js';
 import './enrollAccount.js';
 
 
-
 // Module code
 //
 
 Template.adminLayout.onCreated(function () {
     this.licenseSubs = this.subscribe('license');
-
 });
 
-Template.adminLayout.onDestroyed(function(){
-    if(this.licenseSubs){
+Template.adminLayout.onDestroyed(function () {
+    if (this.licenseSubs) {
         this.licenseSubs.stop();
     }
-});
-
-Template.adminLayout.onRendered(function(){
 });
 
 Template.adminLayout.events({
@@ -64,74 +59,56 @@ Template.adminLayout.events({
         Meteor.logout();
         return false;
     },
-    'click .menu-toggle'(event, template){
-        $("#wrapper").toggleClass("toggled");
+    'click .menu-toggle'(event, template) {
+        $('#wrapper').toggleClass('toggled');
     },
-
-    'click #changeLicenseConfigButton': function(event, template){
+    'click #changeLicenseConfigButton': function (event, template) {
         // Populate the form fields with the data from the current data.
         $('#changeLicenseConfigForm')
-            .find('[name="starter"]').val( Catenis.db.collection.License.findOne({licenseType: "Starter"}).numAllowedDevices ).end()
-            .find('[name="basic"]').val( Catenis.db.collection.License.findOne({licenseType: "Basic"}).numAllowedDevices).end()
-            .find('[name="professional"]').val(Catenis.db.collection.License.findOne({licenseType: "Professional"}).numAllowedDevices).end()
-            .find('[name="enterprise"]').val(Catenis.db.collection.License.findOne({licenseType: "Enterprise"}).numAllowedDevices).end()
+        .find('[name="starter"]').val(Catenis.db.collection.License.findOne({licenseType: 'Starter'}).numAllowedDevices).end()
+        .find('[name="basic"]').val(Catenis.db.collection.License.findOne({licenseType: 'Basic'}).numAllowedDevices).end()
+        .find('[name="professional"]').val(Catenis.db.collection.License.findOne({licenseType: 'Professional'}).numAllowedDevices).end()
+        .find('[name="enterprise"]').val(Catenis.db.collection.License.findOne({licenseType: 'Enterprise'}).numAllowedDevices).end();
     },
-
-
-    'submit #changeLicenseConfigForm'(event, template){
-
+    'submit #changeLicenseConfigForm'(event, template) {
         const form = event.target;
-        let licenseConfig= {};
+        let licenseConfig = {};
 
-        licenseConfig.starter=form.starter.value? form.starter.value: Catenis.db.collection.License.findOne({licenseType: "Starter"}).numAllowedDevices;
-        licenseConfig.basic=form.basic.value?form.basic.value:Catenis.db.collection.License.findOne({licenseType: "Basic"}).numAllowedDevices;
-        licenseConfig.professional=form.professional.value?form.professional.value:Catenis.db.collection.License.findOne({licenseType: "Professional"}).numAllowedDevices;
-        licenseConfig.enterprise=form.enterprise.value?form.enterprise.value:Catenis.db.collection.License.findOne({licenseType: "Enterprise"}).numAllowedDevices;
+        licenseConfig.starter = form.starter.value ? form.starter.value : Catenis.db.collection.License.findOne({licenseType: 'Starter'}).numAllowedDevices;
+        licenseConfig.basic = form.basic.value ? form.basic.value : Catenis.db.collection.License.findOne({licenseType: 'Basic'}).numAllowedDevices;
+        licenseConfig.professional = form.professional.value ? form.professional.value : Catenis.db.collection.License.findOne({licenseType: 'Professional'}).numAllowedDevices;
+        licenseConfig.enterprise = form.enterprise.value ? form.enterprise.value : Catenis.db.collection.License.findOne({licenseType: 'Enterprise'}).numAllowedDevices;
 
         // Call remote method to update client
-            Meteor.call('updateLicenseConfig', licenseConfig, (error) => {
-                if (error) {
+        Meteor.call('updateLicenseConfig', licenseConfig, (error) => {
+            if (error) {
+                console.log('error attempting to update license configuration', error);
+            }
+            else {
+                // Catenis client successfully updated
 
-                    console.log("error attempting to update license configuration", error);
-
-                }else {
-                    // Catenis client successfully updated
-
-                    //close modal form
-                    $('#updateFormModal').modal('hide');
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                }
-            });
+                // Close modal form
+                $('#updateFormModal').modal('hide');
+                $('body').removeClass('modal-open');
+                $('.modal-backdrop').remove();
+            }
+        });
     },
-    'click .sideNavButtons'(event, template){
-        //    change all colors to original color
-        var sideNav= document.getElementsByClassName("sideNavButtons");
+    'click .sideNavButtons'(event, template) {
+        // Change all colors to original color
+        const sideNav = document.getElementsByClassName('sideNavButtons');
 
-        for ( var i=0; i< sideNav.length ; i++ ){
-            sideNav[i].style.backgroundColor = "#e8e9ec";
-            $(sideNav[i]).children()[0].style= "";
-            $(sideNav[i]).children()[1].style= "";
+        for (let i = 0; i < sideNav.length; i++) {
+            sideNav[i].style.backgroundColor = '#e8e9ec';
+            $(sideNav[i]).children()[0].style = '';
+            $(sideNav[i]).children()[1].style = '';
 
             // sideNav[i].style.color = "#333399";
-            sideNav[i].style = "";
+            sideNav[i].style = '';
         }
 
-        (event.currentTarget).style.backgroundColor ="#5555bb";
-        $(event.currentTarget).children()[0].style.color="white";
-        $(event.currentTarget).children()[1].style.color="white";
-
-    },
-
-
-
-
+        (event.currentTarget).style.backgroundColor = '#5555bb';
+        $(event.currentTarget).children()[0].style.color = 'white';
+        $(event.currentTarget).children()[1].style.color = 'white';
+    }
 });
-
-
-Template.adminLayout.helpers({
-
-
-
-});
-
