6426c8c82a69a801b213671ac6a056cc8f1927f1
Cláudio Sá de Castro <claudio@hiades.com.br>
2018-10-09 11:01:04 -0300

    Remove functionality to check consistency of in-memory key store database. This functionality had been added to help debug an issue (detected on the sandbox environment) where blockchain addresses were inadvertently being removed from local key storage. Later it was discovered that it was caused by the functionality used to delete inactive BlockchainAddress objects. A fix was then applied to resolve the issue.

--- meteor_app/imports/server/KeyStore.js
+++ meteor_app/imports/server/KeyStore.js
@@ -170,8 +170,7 @@ const configKeyStore = config.get('keyStore');
 // Configuration settings
 const cfgSettings = {
     obsoleteExtKeyTimeToPurge: configKeyStore.get('obsoleteExtKeyTimeToPurge'),
-    purgeUnusedExtKeyInterval: configKeyStore.get('purgeUnusedExtKeyInterval'),
-    checkDbConsistency: configKeyStore.get('checkDbConsistency')
+    purgeUnusedExtKeyInterval: configKeyStore.get('purgeUnusedExtKeyInterval')
 };
 
 const clientServCredAddrRootTypes = [{
@@ -2983,7 +2982,7 @@ KeyStore.prototype.latestDeviceAssetIssuanceAddressInUse = function (ctnNodeInde
 //      or .bind().
 //
 
-function purgeUnusedExtendedKeys(isInitializing = false) {
+function purgeUnusedExtendedKeys() {
     Catenis.logger.TRACE('Executing process to purge unused HD extended keys from local key storage');
     // Calculate earliest date/time for obsolete HD extended key to have been created/update for it not to be purged
     const obsoleteEarliestTime = new Date(Date.now());
@@ -2993,43 +2992,8 @@ function purgeUnusedExtendedKeys(isInitializing = false) {
 
     // Remove reserved HD extended keys, and HD extended keys that have
     //  turned obsolete for a while
-    const query = {$or: [{isReserved: true}, {$and: [{isObsolete: true}, {'meta.revision': 0}, {'meta.created': {$lt: obsoleteEarliestTimestamp}}]},
-            {$and: [{isObsolete: true}, {'meta.revision': {$gt: 0}}, {'meta.updated': {$lt: obsoleteEarliestTimestamp}}]}]};
-
-    if (!isInitializing && cfgSettings.checkDbConsistency) {
-        // Identify entries that will be removed from in-memory key store database
-        const docsToRemove = this.collExtKey.find(query);
-
-        if (docsToRemove.length > 0) {
-            // Report entries that are about to be removed from in-memory key store database
-            Catenis.logger.DEBUG('Entries about to be removed from in-memory key store database', docsToRemove);
-        }
-    }
-
-    this.collExtKey.findAndRemove(query);
-
-    if (!isInitializing && cfgSettings.checkDbConsistency) {
-        checkDbConsistency.call(this);
-    }
-}
-
-function checkDbConsistency() {
-    Catenis.logger.TRACE('Checking consistency of in-memory key store database');
-    const docsInconsistent = Catenis.db.collection.IssuedBlockchainAddress.find({
-        status: {
-            $in: [
-                'new',
-                'expired'
-            ]
-        },
-        path: {
-            $nin: this.collExtKey.find().map(doc => doc.path)
-        }
-    }).fetch();
-
-    if (docsInconsistent.length > 0) {
-        Catenis.logger.ERROR('In-memory key store database is inconsistent: entries are missing for one or more issued blockchain addresses', docsInconsistent);
-    }
+    this.collExtKey.findAndRemove({$or: [{isReserved: true}, {$and: [{isObsolete: true}, {'meta.revision': 0}, {'meta.created': {$lt: obsoleteEarliestTimestamp}}]},
+            {$and: [{isObsolete: true}, {'meta.revision': {$gt: 0}}, {'meta.updated': {$lt: obsoleteEarliestTimestamp}}]}]});
 }
 
 function storeHDNode(type, path, hdNode, isLeaf, isReserved, isObsolete) {
@@ -3067,7 +3031,7 @@ KeyStore.initialize = function () {
 
     // Execute process to purge unused HD extended keys from local key storage now,
     //  and set recurring timer to execute it periodically
-    purgeUnusedExtendedKeys.call(Catenis.keyStore, true);
+    purgeUnusedExtendedKeys.call(Catenis.keyStore);
     Catenis.logger.TRACE('Setting recurring timer to purge unused HD extended keys from local key storage');
     purgeUnusedExtKeyIntervalHandle = Meteor.setInterval(purgeUnusedExtendedKeys.bind(Catenis.keyStore), cfgSettings.purgeUnusedExtKeyInterval);
 };
