2eadf1a92c3ac8ed701bdcc98e11c77a51129f02
Cláudio Sá de Castro <claudio@hiades.com.br>
2019-03-22 12:21:15 -0300

    Fix how error responses emitted by Parse Request Body middleware are built so they include CORS headers.

--- meteor_app/imports/server/ParseRequestBody.js
+++ meteor_app/imports/server/ParseRequestBody.js
@@ -52,7 +52,7 @@ ParseRequestBody.parser = function (req, res, next) {
     ParseRequestBody.rawBody.parser(req, res, (err) => {
         if (err) {
             Catenis.logger.ERROR('Error parsing request body.', err);
-            sendParsedError(res, err);
+            sendParsedError(req, res, err);
         }
         else {
             if (req._body) {
@@ -68,10 +68,10 @@ ParseRequestBody.parser = function (req, res, next) {
                     catch (err) {
                         Catenis.logger.ERROR('Error parsing request body as JSON.', err);
                         if (err.name === 'SyntaxError') {
-                            sendError(res, 'Request body is not a well-formed JSON', 400);
+                            sendError(req, res, 'Request body is not a well-formed JSON', 400);
                         }
                         else {
-                            sendError(res, 'Internal server error');
+                            sendError(req, res, 'Internal server error');
                         }
                         return;
                     }
@@ -100,26 +100,44 @@ ParseRequestBody.rawBody = {
 // Definition of module (private) functions
 //
 
-function sendError(res, errMsg, statusCode) {
+function sendError(req, res, errMsg, statusCode) {
     res.statusCode = statusCode || 500;
     res.setHeader('Content-Type', 'application/json');
+
+    addCorsResponseHeaders(req, res);
+
     res.end(JSON.stringify({
         status: 'error',
         message: errMsg
     }));
 }
 
-function sendParsedError(res, error) {
+function sendParsedError(req, res, error) {
     if ((error instanceof Error) && (typeof error.status === 'number' || typeof error.statusCode === 'number')) {
         const statusCode = typeof error.status === 'number' ? error.status : error.statusCode;
         // Filter 413 (Request entity too large) status code so a custom message is returned
         const errMsg = statusCode < 500 ? (statusCode === 413 ? 'Request data too large to be processed' : error.message) : 'Internal server error';
 
-        return sendError(res, errMsg, statusCode);
+        return sendError(req, res, errMsg, statusCode);
     }
     else {
-        return sendError(res, 'Internal server error');
+        return sendError(req, res, 'Internal server error');
     }
+}
+
+function addCorsResponseHeaders(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if ('origin' in req.headers) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers['origin']);
+        res.setHeader('Vary', 'Origin');
+    }
+    else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Accept, Origin, Content-Type, X-Bcot-Timestamp, Authorization');
 }
 
 
