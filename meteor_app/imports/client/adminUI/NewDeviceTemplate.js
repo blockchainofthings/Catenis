b20416e2f737d1692764d48e87e01ce28726a849
Cláudio Sá de Castro <claudio@hiades.com.br>
2019-10-09 18:11:41 -0300

    Add feature to ask for confirmation before creating new device.

--- meteor_app/imports/client/adminUI/NewDeviceTemplate.js
+++ meteor_app/imports/client/adminUI/NewDeviceTemplate.js
@@ -24,6 +24,10 @@ import { Catenis } from '../ClientCatenis';
 // Import template UI
 import './NewDeviceTemplate.html';
 
+// Module variables
+const confirmPhrase = 'yes, i do confirm it';
+
+
 // Definition of module (private) functions
 
 function validateFormData(form, errMsgs, template) {
@@ -56,6 +60,8 @@ Template.newDevice.onCreated(function () {
     this.state.set('infoMsg', undefined);
     this.state.set('infoMsgType', 'info');
 
+    this.state.set('displayConfirmNewDeviceSubmitButton', 'none');
+    this.state.set('validatedDeviceInfo', undefined);
     this.state.set('deviceCreated', false);
 });
 
@@ -79,6 +85,77 @@ Template.newDevice.events({
         //      both solutions cause a page reload, whilst clicking on the link does not)
         template.find('#lnkCancel').click();
     },
+    'click #btnConfirmNewDevice'(event, template) {
+        event.preventDefault();
+
+        // Reset action confirmation
+        $('#itxNewDeviceConfirmation')[0].value = '';
+        template.state.set('displayConfirmNewDeviceSubmitButton', 'none');
+    },
+    'hidden.bs.modal #divConfirmNewDevice'(event, template) {
+        // Modal panel has been closed. Make sure that button used to
+        //  activate modal panel is not selected
+        $('#btnCreate').blur();
+
+        template.state.set('validatedDeviceInfo', undefined);
+    },
+    'input #itxNewDeviceConfirmation'(event, template) {
+        // Suppress spaces from beginning of input
+        let inputValue = event.target.value = event.target.value.replace(/^\s+/, '');
+
+        if (inputValue.length > confirmPhrase.length) {
+            // Limit length of input
+            inputValue = event.target.value = inputValue.substring(0, confirmPhrase.length);
+        }
+
+        // Check if input matches confirmation phrase
+        if (inputValue.toLowerCase() === confirmPhrase) {
+            // Show button to confirm action
+            template.state.set('displayConfirmNewDeviceSubmitButton', 'inline');
+        }
+        else {
+            // Hide button to confirm action
+            template.state.set('displayConfirmNewDeviceSubmitButton', 'none');
+        }
+    },
+    'submit #frmConfirmNewDevice'(event, template) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        // Close modal panel
+        $('#divConfirmNewDevice').modal('hide');
+
+        // Disable buttons
+        const btnCancel = template.find('#btnCancel');
+        const btnCreate = template.find('#btnCreate');
+        btnCancel.disabled = true;
+        btnCreate.disabled = true;
+
+        // Display alert message indicating that request is being processed
+        template.state.set('infoMsg', 'Your request is being processed. Please wait.');
+
+        // Call remote method to create new device
+        Meteor.call('createNewDevice', template.data.client_id, template.state.get('validatedDeviceInfo'), (error, deviceId) => {
+            // Reenable buttons
+            btnCancel.disabled = false;
+            btnCreate.disabled = false;
+
+            if (error) {
+                // Clear info alert message, and display error message
+                template.state.set('infoMsg', undefined);
+
+                template.state.set('errMsgs', [
+                    error.toString()
+                ]);
+            }
+            else {
+                // Indicate that device has been successfully created
+                template.state.set('deviceCreated', true);
+                template.state.set('infoMsg', util.format('New device (device ID: %s) successfully created.', deviceId));
+                template.state.set('infoMsgType', 'success');
+            }
+        });
+    },
     'submit #frmNewDevice'(event, template) {
         event.preventDefault();
 
@@ -92,36 +169,7 @@ Template.newDevice.events({
         let deviceInfo;
 
         if ((deviceInfo = validateFormData(form, errMsgs, template))) {
-            // Disable buttons
-            const btnCancel = template.find('#btnCancel');
-            const btnCreate = template.find('#btnCreate');
-            btnCancel.disabled = true;
-            btnCreate.disabled = true;
-
-            // Display alert message indicating that request is being processed
-            template.state.set('infoMsg', 'Your request is being processed. Please wait.');
-
-            // Call remote method to create new device
-            Meteor.call('createNewDevice', template.data.client_id, deviceInfo, (error, deviceId) => {
-                // Reenable buttons
-                btnCancel.disabled = false;
-                btnCreate.disabled = false;
-
-                if (error) {
-                    // Clear info alert message, and display error message
-                    template.state.set('infoMsg', undefined);
-
-                    template.state.set('errMsgs', [
-                        error.toString()
-                    ]);
-                }
-                else {
-                    // Indicate that device has been successfully created
-                    template.state.set('deviceCreated', true);
-                    template.state.set('infoMsg', util.format('New device (device ID: %s) successfully created.', deviceId));
-                    template.state.set('infoMsgType', 'success');
-                }
-            });
+            template.state.set('validatedDeviceInfo', deviceInfo);
         }
         else {
             template.state.set('errMsgs', errMsgs);
@@ -155,5 +203,11 @@ Template.newDevice.helpers({
     },
     showCreateButton() {
         return !Template.instance().state.get('deviceCreated');
+    },
+    displayConfirmNewDeviceSubmitButton() {
+        return Template.instance().state.get('displayConfirmNewDeviceSubmitButton');
+    },
+    validatedDeviceInfo() {
+        return Template.instance().state.get('validatedDeviceInfo');
     }
 });
--- meteor_app/imports/client/clientUI/ClientNewDeviceTemplate.js
+++ meteor_app/imports/client/clientUI/ClientNewDeviceTemplate.js
@@ -24,6 +24,9 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 // Import template UI
 import './ClientNewDeviceTemplate.html';
 
+// Module variables
+const confirmPhrase = 'yes, i do confirm it';
+
 
 // Definition of module (private) functions
 
@@ -57,6 +60,8 @@ Template.clientNewDevice.onCreated(function () {
     this.state.set('infoMsg', undefined);
     this.state.set('infoMsgType', 'info');
 
+    this.state.set('displayConfirmNewDeviceSubmitButton', 'none');
+    this.state.set('validatedDeviceInfo', undefined);
     this.state.set('deviceCreated', false);
 });
 
@@ -80,6 +85,77 @@ Template.clientNewDevice.events({
         //      both solutions cause a page reload, whilst clicking on the link does not)
         template.find('#lnkCancel').click();
     },
+    'click #btnConfirmNewDevice'(event, template) {
+        event.preventDefault();
+
+        // Reset action confirmation
+        $('#itxNewDeviceConfirmation')[0].value = '';
+        template.state.set('displayConfirmNewDeviceSubmitButton', 'none');
+    },
+    'hidden.bs.modal #divConfirmNewDevice'(event, template) {
+        // Modal panel has been closed. Make sure that button used to
+        //  activate modal panel is not selected
+        $('#btnCreate').blur();
+
+        template.state.set('validatedDeviceInfo', undefined);
+    },
+    'input #itxNewDeviceConfirmation'(event, template) {
+        // Suppress spaces from beginning of input
+        let inputValue = event.target.value = event.target.value.replace(/^\s+/, '');
+
+        if (inputValue.length > confirmPhrase.length) {
+            // Limit length of input
+            inputValue = event.target.value = inputValue.substring(0, confirmPhrase.length);
+        }
+
+        // Check if input matches confirmation phrase
+        if (inputValue.toLowerCase() === confirmPhrase) {
+            // Show button to confirm action
+            template.state.set('displayConfirmNewDeviceSubmitButton', 'inline');
+        }
+        else {
+            // Hide button to confirm action
+            template.state.set('displayConfirmNewDeviceSubmitButton', 'none');
+        }
+    },
+    'submit #frmConfirmNewDevice'(event, template) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        // Close modal panel
+        $('#divConfirmNewDevice').modal('hide');
+
+        // Disable buttons
+        const btnCancel = template.find('#btnCancel');
+        const btnCreate = template.find('#btnCreate');
+        btnCancel.disabled = true;
+        btnCreate.disabled = true;
+
+        // Display alert message indicating that request is being processed
+        template.state.set('infoMsg', 'Your request is being processed. Please wait.');
+
+        // Call remote method to create new device
+        Meteor.call('createCurrentClientNewDevice', template.state.get('validatedDeviceInfo'), (error, deviceId) => {
+            // Reenable buttons
+            btnCancel.disabled = false;
+            btnCreate.disabled = false;
+
+            if (error) {
+                // Clear info alert message, and display error message
+                template.state.set('infoMsg', undefined);
+
+                template.state.set('errMsgs', [
+                    error.toString()
+                ]);
+            }
+            else {
+                // Indicate that device has been successfully created
+                template.state.set('deviceCreated', true);
+                template.state.set('infoMsg', util.format('New device (device ID: %s) successfully created.', deviceId));
+                template.state.set('infoMsgType', 'success');
+            }
+        });
+    },
     'submit #frmNewDevice'(event, template) {
         event.preventDefault();
 
@@ -93,36 +169,7 @@ Template.clientNewDevice.events({
         let deviceInfo;
 
         if ((deviceInfo = validateFormData(form, errMsgs, template))) {
-            // Disable buttons
-            const btnCancel = template.find('#btnCancel');
-            const btnCreate = template.find('#btnCreate');
-            btnCancel.disabled = true;
-            btnCreate.disabled = true;
-
-            // Display alert message indicating that request is being processed
-            template.state.set('infoMsg', 'Your request is being processed. Please wait.');
-
-            // Call remote method to create new device
-            Meteor.call('createCurrentClientNewDevice', deviceInfo, (error, deviceId) => {
-                // Reenable buttons
-                btnCancel.disabled = false;
-                btnCreate.disabled = false;
-
-                if (error) {
-                    // Clear info alert message, and display error message
-                    template.state.set('infoMsg', undefined);
-
-                    template.state.set('errMsgs', [
-                        error.toString()
-                    ]);
-                }
-                else {
-                    // Indicate that device has been successfully created
-                    template.state.set('deviceCreated', true);
-                    template.state.set('infoMsg', util.format('New device (device ID: %s) successfully created.', deviceId));
-                    template.state.set('infoMsgType', 'success');
-                }
-            });
+            template.state.set('validatedDeviceInfo', deviceInfo);
         }
         else {
             template.state.set('errMsgs', errMsgs);
@@ -156,5 +203,11 @@ Template.clientNewDevice.helpers({
     },
     showCreateButton() {
         return !Template.instance().state.get('deviceCreated');
+    },
+    displayConfirmNewDeviceSubmitButton() {
+        return Template.instance().state.get('displayConfirmNewDeviceSubmitButton');
+    },
+    validatedDeviceInfo() {
+        return Template.instance().state.get('validatedDeviceInfo');
     }
 });
