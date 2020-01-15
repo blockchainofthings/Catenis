96c6682429524f331497dd6e98cb1c0d5c624200
Cláudio Sá de Castro <claudio@hiades.com.br>
2020-01-15 14:03:08 -0300

    Revert changes to IpfsClient module that added new methods since those new methods are not in use; they are used by Catenis Off-Chain Server instead.

--- meteor_app/imports/server/IpfsClient.js
+++ meteor_app/imports/server/IpfsClient.js
@@ -42,26 +42,12 @@ export function IpfsClient(host, port, protocol) {
         protocol: protocol
     });
 
-    addMissingIpfsMethods.call(this);
-
     // noinspection JSUnresolvedVariable
     this.api = {
         add: Meteor.wrapAsync(this.ipfs.add, this.ipfs),
         cat: Meteor.wrapAsync(this.ipfs.cat, this.ipfs),
         catReadableStream: this.ipfs.catReadableStream,
-        id: Meteor.wrapAsync(this.ipfs.id, this.ipfs),
-        ls: Meteor.wrapAsync(this.ipfs.ls, this.ipfs),
-        files: {
-            ls: Meteor.wrapAsync(this.ipfs.files.ls, this.ipfs),
-            mkdir: Meteor.wrapAsync(this.ipfs.files.mkdir, this.ipfs),
-            stat: Meteor.wrapAsync(this.ipfs.files.stat, this.ipfs),
-            write: Meteor.wrapAsync(this.ipfs.files.write, this.ipfs)
-        },
-        pin: {
-            add: Meteor.wrapAsync(this.ipfs.pin.add, this.ipfs),
-            update: Meteor.wrapAsync(this.ipfs.pin.update, this.ipfs),
-            rm: Meteor.wrapAsync(this.ipfs.pin.rm, this.ipfs)
-        }
+        id: Meteor.wrapAsync(this.ipfs.id, this.ipfs)
     };
 }
 
@@ -105,78 +91,6 @@ IpfsClient.prototype.id = function () {
     }
 };
 
-IpfsClient.prototype.ls = function (ipfsPath) {
-    try {
-        return this.api.ls(ipfsPath);
-    }
-    catch (err) {
-        handleError('ls', err);
-    }
-};
-
-IpfsClient.prototype.filesLs = function (path, options) {
-    try {
-        return this.api.files.ls(path, options);
-    }
-    catch (err) {
-        handleError('filesLs', err);
-    }
-};
-
-IpfsClient.prototype.filesMkdir = function (path, options) {
-    try {
-        return this.api.files.mkdir(path, options);
-    }
-    catch (err) {
-        handleError('filesMkdir', err);
-    }
-};
-
-IpfsClient.prototype.filesStat = function (path, options) {
-    try {
-        return this.api.files.stat(path, options);
-    }
-    catch (err) {
-        handleError('filesStat', err);
-    }
-};
-
-IpfsClient.prototype.filesWrite = function (path, content, options) {
-    try {
-        return this.api.files.write(path, content, options);
-    }
-    catch (err) {
-        handleError('filesWrite', err);
-    }
-};
-
-IpfsClient.prototype.pinAdd = function (hash, options) {
-    try {
-        return this.api.pin.add(hash, options);
-    }
-    catch (err) {
-        handleError('pinAdd', err);
-    }
-};
-
-IpfsClient.prototype.pinUpdate = function (fromHash, toHash, options) {
-    try {
-        return this.api.pin.update(fromHash, toHash, options);
-    }
-    catch (err) {
-        handleError('pinUpdate', err);
-    }
-};
-
-IpfsClient.prototype.pinRm = function (hash, options) {
-    try {
-        return this.api.pin.rm(hash, options);
-    }
-    catch (err) {
-        handleError('pinRm', err);
-    }
-};
-
 
 // Module functions used to simulate private IpfsClient object methods
 //  NOTE: these functions need to be bound to a IpfsClient object reference (this) before
@@ -184,30 +98,8 @@ IpfsClient.prototype.pinRm = function (hash, options) {
 //      or .bind().
 //
 
-function addMissingIpfsMethods() {
-    addMissingPinUpdateMethod.call(this);
-}
-
-function addMissingPinUpdateMethod() {
-    this.ipfs.pin.update = ((send) => {
-        return function (hash1, hash2, opts, callback) {
-            if (typeof opts === 'function') {
-                callback = opts;
-                opts = null;
-            }
-            send({
-                path: 'pin/update',
-                args: [hash1, hash2],
-                qs: opts
-            }, (err, res) => {
-                if (err) {
-                    return callback(err)
-                }
-                callback(null, res.Pins.map((hash) => ({ hash: hash })))
-            })
-        };
-    })(this.ipfs.send);
-}
+/*function priv_func() {
+}*/
 
 
 // IpfsClient function class (public) methods
