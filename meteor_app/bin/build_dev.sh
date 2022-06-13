6e31dfb51937f0132b1be60730ffc4e73978a1c0
Cláudio Sá de Castro <claudio@hiades.com.br>
2022-06-13 16:52:27 -0300

    Update build scripts so that they run with no error on a Mac with Apple Silicon processor. Background: running the build script on a Mac with Apple Silicon processor would produce the following error: Error: missing tool for os.osx.x86_64 in meteor-tool@2.5.1.

--- meteor_app/bin/build_dev.sh
+++ meteor_app/bin/build_dev.sh
@@ -5,4 +5,4 @@ if [ -f ../build/dev/Catenis_dev-$npm_package_version.tar.gz ]; then
     exit 1
 fi
 
-meteor build ../build/dev/ && echo -n "Adding config dir to server tarball..." && mkdir ../build/dev/__temp && mkdir ../build/dev/__temp/bundle && ln -s -F ../../../../meteor_app/config ../build/dev/__temp/bundle/config && tar -cHzf ../build/dev/Catenis_dev-$npm_package_version.tar.gz --exclude .DS_Store @../build/dev/meteor_app.tar.gz -C../build/dev/__temp bundle/config && echo " Done" && rm -r ../build/dev/__temp ../build/dev/meteor_app.tar.gz
+arch -arm64 meteor build ../build/dev/ && echo -n "Adding config dir to server tarball..." && mkdir ../build/dev/__temp && mkdir ../build/dev/__temp/bundle && ln -s -F ../../../../meteor_app/config ../build/dev/__temp/bundle/config && tar -cHzf ../build/dev/Catenis_dev-$npm_package_version.tar.gz --exclude .DS_Store @../build/dev/meteor_app.tar.gz -C../build/dev/__temp bundle/config && echo " Done" && rm -r ../build/dev/__temp ../build/dev/meteor_app.tar.gz
