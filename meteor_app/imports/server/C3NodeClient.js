a196a23d06dc9e2b24b269e51ec71686188d77d2
Cláudio Sá de Castro <claudio@hiades.com.br>
2022-06-21 09:14:28 -0300

    Replace HTTP library used within the C3NodeClient module. Rationale: the new library, Got, provides better support for retrieving the response body via a (readable) stream, a feature that we needed to implement the new API methods getAssetMetadataReadableStream and getNFTokenMetadataReadableStream.

--- meteor_app/imports/server/C3NodeClient.js
+++ meteor_app/imports/server/C3NodeClient.js
@@ -11,18 +11,18 @@
 //
 // Internal node modules
 import util from 'util';
-import http from 'http';
 // Third-party node modules
 import config from 'config';
+import got from 'got';
 // Meteor packages
 import { Meteor } from 'meteor/meteor';
+import { Promise } from 'meteor/promise';
 
 // References code in other (Catenis) modules
 import { Catenis } from './Catenis';
 
 // Config entries
 const c3NodeClientConfig = config.get('c3NodeClient');
-const methodPathConfig = c3NodeClientConfig.get('methodPath');
 
 // Configuration settings
 const cfgSettings = {
@@ -63,19 +63,29 @@ const cfgSettings = {
 
 // C3NodeClient function class
 export function C3NodeClient(network, host, inetAddr, username, password, timeout) {
-    this.host = host;
-    this.port = network === 'testnet' || network === 'regtest' ? cfgSettings.testnetTcpPort : cfgSettings.mainTcpPort;
-    this.timeout = timeout;
+    const apiUrl = new URL(`http://${host}`);
+    apiUrl.port = network === 'testnet' || network === 'regtest' ? cfgSettings.testnetTcpPort : cfgSettings.mainTcpPort;
+
+    this.httpOptions = {
+        prefixUrl: apiUrl.toString(),
+        retry: 0
+    };
+
+    if (timeout) {
+        this.httpOptions.timeout = {
+            socket: timeout,
+            response: timeout
+        };
+    }
 
     if (inetAddr.length > 0) {
-        this.inetAddr = inetAddr;
+        this.httpOptions.localAddress = inetAddr;
     }
 
     if (username.length > 0) {
-        this.auth = username + ':' + password;
+        this.httpOptions.username = username;
+        this.httpOptions.password = password;
     }
-
-    this.syncSendRequest = Meteor.wrapAsync(sendRequest, this);
 }
 
 
@@ -87,7 +97,9 @@ export function C3NodeClient(network, host, inetAddr, username, password, timeou
 //
 C3NodeClient.prototype.parseNow = function () {
     try {
-        return getRequest.call(this, cfgSettings.methodPath.parseNow);
+        return Promise.await(
+            parseJsonResponse(got(cfgSettings.methodPath.parseNow, this.httpOptions))
+        );
     }
     catch (err) {
         handleError('parseNow', err);
@@ -115,7 +127,12 @@ C3NodeClient.prototype.getAddressesUtxos = function (addresses, numOfConfirmatio
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAddressesUtxos, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAddressesUtxos, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAddressesUtxos', err);
@@ -146,7 +163,12 @@ C3NodeClient.prototype.getUtxos = function (utxos, numOfConfirmations, waitForPa
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getUtxos, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getUtxos, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getUtxos', err);
@@ -172,7 +194,12 @@ C3NodeClient.prototype.getTxouts = function (txouts, waitForParsing = true) {
     }
     
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getTxouts, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getTxouts, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getTxouts', err);
@@ -195,7 +222,12 @@ C3NodeClient.prototype.getAddressesTransactions = function (addresses, waitForPa
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAddressesTransactions, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAddressesTransactions, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAddressesTransactions', err);
@@ -212,7 +244,12 @@ C3NodeClient.prototype.transmit = function (txHex) {
     };
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.transmit, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.transmit, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('transmit', err);
@@ -223,7 +260,9 @@ C3NodeClient.prototype.transmit = function (txHex) {
 //
 C3NodeClient.prototype.getInfo = function () {
     try {
-        return getRequest.call(this, cfgSettings.methodPath.getInfo);
+        return Promise.await(
+            parseJsonResponse(got(cfgSettings.methodPath.getInfo, this.httpOptions))
+        );
     }
     catch (err) {
         handleError('getInfo', err);
@@ -245,7 +284,12 @@ C3NodeClient.prototype.importAddresses = function (addresses, reindex) {
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.importAddresses, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.importAddresses, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('importAddresses', err);
@@ -274,7 +318,12 @@ C3NodeClient.prototype.getAssetHolders = function (ccAssetId, numOfConfirmations
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAssetHolders, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAssetHolders, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAssetHolders', err);
@@ -307,7 +356,12 @@ C3NodeClient.prototype.getAssetBalance = function (ccAssetId, addresses, numOfCo
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAssetBalance, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAssetBalance, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAssetBalance', err);
@@ -340,7 +394,12 @@ C3NodeClient.prototype.getMultiAssetBalance = function (ccAssetIds, addresses, n
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getMultiAssetBalance, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getMultiAssetBalance, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getMultiAssetBalance', err);
@@ -363,7 +422,12 @@ C3NodeClient.prototype.getAssetIssuance = function (ccAssetId, waitForParsing = 
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAssetIssuance, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAssetIssuance, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAssetIssuance', err);
@@ -386,7 +450,12 @@ C3NodeClient.prototype.getAssetIssuingAddress = function (ccAssetId, waitForPars
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAssetIssuingAddress, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAssetIssuingAddress, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAssetIssuingAddress', err);
@@ -416,7 +485,12 @@ C3NodeClient.prototype.getOwningAssets = function (addresses, numOfConfirmations
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getOwningAssets, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getOwningAssets, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getOwningAssets', err);
@@ -440,10 +514,48 @@ C3NodeClient.prototype.getAssetMetadata = function (assetId, waitForParsing = tr
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAssetMetadata, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAssetMetadata, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAssetMetadata', err);
+    }
+}
+
+/**
+ * @external {Readable} https://nodejs.org/api/stream.html#class-streamreadable
+ */
+
+/**
+ * Call Catenis Colored Coins node server getAssetMetadata method returning a readable stream
+ * @param {string} assetId The Colored Coins ID of the asset to retrieve the metadata
+ * @param {boolean} [waitForParsing=true] Indicates whether processing of request should wait until parsing of assets
+ *                                         is complete
+ * @returns {Readable} A readable stream used to retrieve the JSON object representing the asset metadata, or undefined
+ *                      if no metadata is found
+ */
+C3NodeClient.prototype.getAssetMetadataReadableStream = function (assetId, waitForParsing = true) {
+    const postData = {
+        assetId
+    };
+
+    if (waitForParsing) {
+        postData.waitForParsing = true;
+    }
+
+    try {
+        // noinspection JSValidateTypes
+        return got.stream.post(cfgSettings.methodPath.getAssetMetadata, {
+            ...this.httpOptions,
+            json: postData
+        });
+    }
+    catch (err) {
+        handleError('getAssetMetadataReadableStream', err);
     }
 }
 
@@ -465,10 +577,44 @@ C3NodeClient.prototype.getNFTokenMetadata = function (tokenId, waitForParsing = 
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getNFTokenMetadata, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getNFTokenMetadata, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getNFTokenMetadata', err);
+    }
+}
+
+/**
+ * Call Catenis Colored Coins node server getNFTokenMetadata method returning a readable stream
+ * @param {string} tokenId The Colored Coins ID of the non-fungible asset token to retrieve the metadata
+ * @param {boolean} [waitForParsing=true] Indicates whether processing of request should wait until parsing of assets
+ *                                         is complete
+ * @returns {Readable} A readable stream used to retrieve the  JSON object representing the non-fungible asset token
+ *                      metadata, or undefined if no metadata is found
+ */
+C3NodeClient.prototype.getNFTokenMetadataReadableStream = function (tokenId, waitForParsing = true) {
+    const postData = {
+        tokenId
+    };
+
+    if (waitForParsing) {
+        postData.waitForParsing = true;
+    }
+
+    try {
+        // noinspection JSValidateTypes
+        return got.stream.post(cfgSettings.methodPath.getNFTokenMetadata, {
+            ...this.httpOptions,
+            json: postData
+        });
+    }
+    catch (err) {
+        handleError('getNFTokenMetadataReadableStream', err);
     }
 }
 
@@ -490,7 +636,12 @@ C3NodeClient.prototype.getNFTokenAsset = function (tokenId, waitForParsing = tru
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getNFTokenAsset, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getNFTokenAsset, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getNFTokenAsset', err);
@@ -530,7 +681,12 @@ C3NodeClient.prototype.getNFTokenOwner = function (tokenId, numOfConfirmations =
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getNFTokenOwner, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getNFTokenOwner, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getNFTokenOwner', err);
@@ -567,7 +723,12 @@ C3NodeClient.prototype.getAllNFTokensOwner = function (assetId, numOfConfirmatio
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getAllNFTokensOwner, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getAllNFTokensOwner, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getAllNFTokensOwner', err);
@@ -617,7 +778,12 @@ C3NodeClient.prototype.getOwnedNFTokens = function (addresses, assetId, numOfCon
     }
 
     try {
-        return postRequest.call(this, cfgSettings.methodPath.getOwnedNFTokens, postData);
+        return Promise.await(
+            parseJsonResponse(got.post(cfgSettings.methodPath.getOwnedNFTokens, {
+                ...this.httpOptions,
+                json: postData
+            }))
+        );
     }
     catch (err) {
         handleError('getOwnedNFTokens', err);
@@ -630,136 +796,6 @@ C3NodeClient.prototype.getOwnedNFTokens = function (addresses, assetId, numOfCon
 //      they are called, by means of one of the predefined function methods .call(), .apply()
 //      or .bind().
 //
-
-function getRequest(path) {
-    return this.syncSendRequest('GET', path, null);
-}
-
-function postRequest(path, data) {
-    return this.syncSendRequest('POST', path, JSON.stringify(data));
-}
-
-// Sends an HTTP request and processes its response
-//
-//  Arguments:
-//   method [String] - The method/verb of the request; either 'GET' or 'POST'
-//   path [String]   - The url path of the request
-//   body [String] - (optional) Contents of the body of the request to send
-//   callback [Function(err, res)] - The callback function to receive the request outcome
-function sendRequest(method, path, body, callback) {
-    const reqOpts = {
-        hostname: this.host,
-        port: this.port,
-        method: method,
-        path: path,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    if (this.inetAddr) {
-        reqOpts.localAddress = this.inetAddr;
-    }
-
-    if (this.auth) {
-        reqOpts.auth = this.auth;
-    }
-
-    if (body) {
-        reqOpts.headers['Content-Length'] = Buffer.byteLength(body);
-    }
-
-    let reqEnded = false;
-
-    // Prepare request
-    const req = http.request(reqOpts);
-
-    // Set request time out
-    const reqTimeout = setTimeout(() => {
-        if (!reqEnded) {
-            reqEnded = true;
-
-            // Abort request and return error
-            req.abort();
-
-            callback(new Error('ETIMEDOUT'));
-        }
-    }, cfgSettings.connectionTimeout);
-
-    // Set additional timeout on socket in case of remote freeze after sending headers
-    req.setTimeout(cfgSettings.connectionTimeout, () => {
-        if (!reqEnded) {
-            reqEnded = true;
-
-            // Abort request and return error
-            req.abort();
-
-            callback(new Error('ESOCKETTIMEDOUT'));
-        }
-    });
-
-    req.on('error', (err) => {
-        if (!reqEnded) {
-            reqEnded = true;
-
-            // Stop timeout and return error
-            clearTimeout(reqTimeout);
-
-            callback(err);
-        }
-    });
-
-    req.on('response', (res) => {
-        // Stop timeout
-        clearTimeout(reqTimeout);
-
-        let receivedData = '';
-
-        res.on('data', (chunk) => {
-            receivedData += chunk;
-        });
-
-        res.on('end', () => {
-            if (!reqEnded) {
-                reqEnded = true;
-
-                if (res.statusCode !== 200) {
-                    // Response error. Return error
-                    const err = new Error(util.format('Response error: [%d] - %s', res.statusCode, res.statusMessage));
-                    err.code = res.statusCode;
-                    err.body = receivedData;
-
-                    callback(err);
-                }
-                else {
-                    if (receivedData.length > 0) {
-                        // Try to parse received data
-                        let jsonData;
-
-                        try {
-                            jsonData = JSON.parse(receivedData);
-                        }
-                        catch (err) {
-                            // Error trying to parse returned data. Return error
-                            callback(new Error(util.format('Error parsing returned data: [%s] - %s', err.name, err.message)));
-                            return;
-                        }
-
-                        // Return parsed data
-                        callback(undefined, jsonData);
-                    }
-                    else {
-                        // Just indicate that request has returned
-                        callback();
-                    }
-                }
-            }
-        });
-    });
-
-    // Send request
-    req.end(body ? body : undefined);
-}
 
 
 // C3NodeClient function class (public) methods
@@ -775,11 +811,27 @@ C3NodeClient.initialize = function () {
 // C3NodeClient function class (public) properties
 //
 
-/*C3NodeClient.prop = {};*/
-
 
 // Definition of module (private) functions
 //
+
+/**
+ * Parse an HTTP response as a JSON object
+ * @param {*} gotReturn Value returned from a got library HTTP request method
+ * @returns {Promise<any>} A promise that resolves to the parsed JSON object or undefined if the response is empty
+ */
+async function parseJsonResponse(gotReturn) {
+    const retBuf = await gotReturn.buffer();
+
+    if (retBuf.length > 0) {
+        try {
+            return JSON.parse(retBuf.toString());
+        }
+        catch (err) {
+            throw new Error(`Error parsing returned data: [${err.name}] - ${err.message}`);
+        }
+    }
+}
 
 function handleError(methodName, err) {
     let errMsg = util.format('Error calling Catenis Colored Coins node server \'%s\' method.', methodName);
@@ -791,7 +843,6 @@ function handleError(methodName, err) {
 
     throw new Meteor.Error('ctn_c3_node_svr_error', errMsg, err);
 }
-
 
 
 // Module code
